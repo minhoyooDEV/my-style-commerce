712e851307374afd6ed0b72362d2dd0bb789a9ba
minho.yoo <minho.yoo.dev@gmail.com>
2021-04-19 17:07:21 +0900

    기본 스타일 설정값 추가

--- styled/styled.d.ts
+++ styled/styled.d.ts
@@ -4,8 +4,23 @@ declare module 'styled-components' {
 	export interface DefaultTheme extends BasicTheme {
 		borderRadius: string;
 		spacing: string;
+		'spacing-2': string;
 		contentsMaxWith: string;
 
 		headerHeight: string;
+		colors: {
+			main: string;
+			sub: string;
+			pink: string;
+			orange: string;
+			green: string;
+			yellow: string;
+			grey06: string;
+			grey20: string;
+			grey40: string;
+			grey60: string;
+			grey80: string;
+			black: string;
+		};
 	}
 }
--- styled/themes.ts
+++ styled/themes.ts
@@ -5,9 +5,24 @@ const defaultSpacing = '0.5rem';
 const basicTheme: BasicTheme = {
 	borderRadius: '0.5rem',
 	spacing: defaultSpacing,
-	contentsMaxWith: '1200px',
+	'spacing-2': `calc(${defaultSpacing} * 2)`,
+	contentsMaxWith: '768px',
 
 	headerHeight: `3rem`,
+	colors: {
+		main: '#fc676c',
+		sub: '#eda93d',
+		pink: '#fc676c',
+		orange: '#eda93d',
+		green: '#4bef26',
+		yellow: '#f9d936',
+		grey06: '#EEE',
+		grey20: '#CCC',
+		grey40: '#999',
+		grey60: '#666',
+		grey80: '#333',
+		black: '#000',
+	},
 };
 
 export { basicTheme };
