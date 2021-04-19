803d7a64194efbeaedb6c4f1504dbf83f8093eb8
myno <minho.yoo.dev@gmail.com>
2021-04-19 21:53:00 +0900

    헤더 버튼 링크 추가

--- components/app/Header.tsx
+++ components/app/Header.tsx
@@ -2,6 +2,7 @@ import ContainerStyled from '../../styled/container.styled';
 import HeaderButtonGroupStyled from '../../styled/header-button-group.styled';
 import HeaderButtonStyled from '../../styled/header-button.styled';
 import HeaderStyled from '../../styled/header.styled';
+import Link from 'next/link';
 
 const Header = () => (
 	<HeaderStyled>
@@ -9,8 +10,12 @@ const Header = () => (
 			<div>오늘 뭐입지?</div>
 			<nav>
 				<HeaderButtonGroupStyled>
-					<HeaderButtonStyled>상품</HeaderButtonStyled>
-					<HeaderButtonStyled>랭킹</HeaderButtonStyled>
+					<Link href="/">
+						<HeaderButtonStyled>상품</HeaderButtonStyled>
+					</Link>
+					<Link href="/ranks">
+						<HeaderButtonStyled>랭킹</HeaderButtonStyled>
+					</Link>
 				</HeaderButtonGroupStyled>
 			</nav>
 		</ContainerStyled>
