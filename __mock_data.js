1476893c9e6f24ef29ab43c382e0718b29becce9
minho.yoo <minho.yoo.dev@gmail.com>
2021-04-19 15:14:54 +0900

    mockdata 추가

---
+++ __mock_data.js
@@ -0,0 +1,301 @@
+export const products = {
+	data: [
+		{
+			id: 'bantong_13519',
+			image: {
+				id: 41316547,
+				url: 'https://usercontents-c.styleshare.kr/images/41316547/original',
+			},
+			liked: false,
+			likedCount: 0,
+			mobileUrl:
+				'http://bantong.co.kr/m/goods/view.php?goodsno=13519&inflow=naver&utm_source=styleshare&cafe_mkt=styleshare',
+			price: 11000,
+			shopId: 'bantong',
+			shopName: '\ubc18\ud2f0\ub300\ud1b5\ub839',
+			title: '\ubc18\ud2f0 \ub9e8\ud22c\ub9e8 \ud551\ud06c\uc138\ud2b8 (\uc0c1\uc758\uae30\uc900)',
+			url:
+				'http://bantong.co.kr/shop/goods/goods_view.php?goodsno=13519&inflow=naver&utm_source=styleshare&cafe_mkt=styleshare',
+		},
+		{
+			id: 'jtamtam_5393',
+			image: {
+				id: 40995323,
+				url: 'https://usercontents-c.styleshare.kr/images/40995323/original',
+			},
+			liked: false,
+			likedCount: 0,
+			mobileUrl:
+				'http://m.jtamtam.co.kr/product/detail.html?product_no=5393&cate_no=4&display_group=1&utm_source=styleshare&cafe_mkt=styleshare',
+			price: 13500,
+			shopId: 'jtamtam',
+			shopName: '\uc81c\uc774\ud0d0\ud0d0',
+			title: '\ub2e8\uac00\ub77c \ud2b8\uc784 t (3 color)',
+			url:
+				'http://jtamtam.co.kr/product/detail.html?product_no=5393&cate_no=4&display_group=1&utm_source=styleshare&cafe_mkt=styleshare',
+		},
+		{
+			id: 'bantong_13486',
+			image: {
+				id: 41298777,
+				url: 'https://usercontents-c.styleshare.kr/images/41298777/original',
+			},
+			liked: false,
+			likedCount: 0,
+			mobileUrl:
+				'http://bantong.co.kr/m/goods/view.php?goodsno=13486&inflow=naver&utm_source=styleshare&cafe_mkt=styleshare',
+			price: 20000,
+			shopId: 'bantong',
+			shopName: '\ubc18\ud2f0\ub300\ud1b5\ub839',
+			title: '\ubc18\ud2f0 \ubc30\uad6c\ubcf5 \ubbfc\ud2b8\uc138\ud2b8 (\uc138\ud2b8\uae30\uc900)',
+			url:
+				'http://bantong.co.kr/shop/goods/goods_view.php?goodsno=13486&inflow=naver&utm_source=styleshare&cafe_mkt=styleshare',
+		},
+		{
+			id: 'bantong_13526',
+			image: {
+				id: 41316570,
+				url: 'https://usercontents-c.styleshare.kr/images/41316570/original',
+			},
+			liked: false,
+			likedCount: 0,
+			mobileUrl:
+				'http://bantong.co.kr/m/goods/view.php?goodsno=13526&inflow=naver&utm_source=styleshare&cafe_mkt=styleshare',
+			price: 6000,
+			shopId: 'bantong',
+			shopName: '\ubc18\ud2f0\ub300\ud1b5\ub839',
+			title:
+				'\ubc18\ud2f0 \ub300\ud55c\ubbfc\uad6d \ud574\uad70 \uae34\ud314\uc138\ud2b8(\uc0c1\uc758\uae30\uc900)  ',
+			url:
+				'http://bantong.co.kr/shop/goods/goods_view.php?goodsno=13526&inflow=naver&utm_source=styleshare&cafe_mkt=styleshare',
+		},
+		{
+			id: 'coolpis_176108',
+			image: {
+				id: 41226832,
+				url: 'https://usercontents-c.styleshare.kr/images/41226832/original',
+			},
+			liked: false,
+			likedCount: 0,
+			mobileUrl:
+				'http://www.coolpis.co.kr/m/product.html?branduid=176108&utm_source=styleshare&cafe_mkt=styleshare',
+			price: 19000,
+			shopId: 'coolpis',
+			shopName: '\ud5ec\ub85c\uc6b0\ucfe8\ud53c\uc2a4',
+			title:
+				'\uac00\uc744\uc2e0\uc0c1\uc138\uc77c \uc624\ub298\uae4c\uc9c0 29.000\uc6d0\u219219.000\uc6d0 "\ubca0\uc774\ub860\uc2a4\ud2b8\ub77c\uc774\ud504\ud5e8\ub9ac\ub125\ub2c8\ud2b8(3\uceec\ub7ec)"',
+			url:
+				'http://www.coolpis.co.kr/index.html?branduid=176108&utm_source=styleshare&cafe_mkt=styleshare',
+		},
+		{
+			id: 'yourheartbadge_1215',
+			image: {
+				id: 40669235,
+				url: 'https://usercontents-c.styleshare.kr/images/40669235/original',
+			},
+			liked: false,
+			likedCount: 0,
+			mobileUrl:
+				'http://m.yourheartbadge.co.kr/product/detail.html?product_no=1215&cate_no=24&display_group=1&utm_source=styleshare&cafe_mkt=styleshare',
+			price: 15000,
+			shopId: 'yourheartbadge',
+			shopName: '\ub2c8\ub9d8\uc744\ube8f\uc9c0',
+			title: 'A Sunday Afternoon on the Island of La Grande Jatte',
+			url:
+				'http://yourheartbadge.co.kr/product/detail.html?product_no=1215&cate_no=24&display_group=1&utm_source=styleshare&cafe_mkt=styleshare',
+		},
+		{
+			id: 'j_blin_4389',
+			image: {
+				id: 40955010,
+				url: 'https://usercontents-c.styleshare.kr/images/40955010/original',
+			},
+			liked: false,
+			likedCount: 0,
+			mobileUrl:
+				'http://m.j-blin.com/product/detail.html?product_no=4389&cate_no=47&display_group=1&utm_source=styleshare&cafe_mkt=styleshare',
+			price: 11500,
+			shopId: 'j_blin',
+			shopName: '\uc81c\uc774\ube14\ub9b0',
+			title: '\ub2f5\uc2a4 \uadc0\uac78\uc774',
+			url:
+				'http://j-blin.com/product/detail.html?product_no=4389&cate_no=47&display_group=1&utm_source=styleshare&cafe_mkt=styleshare',
+		},
+		{
+			id: 'j_blin_4391',
+			image: {
+				id: 40955015,
+				url: 'https://usercontents-c.styleshare.kr/images/40955015/original',
+			},
+			liked: false,
+			likedCount: 0,
+			mobileUrl:
+				'http://m.j-blin.com/product/detail.html?product_no=4391&cate_no=54&display_group=1&utm_source=styleshare&cafe_mkt=styleshare',
+			price: 29000,
+			shopId: 'j_blin',
+			shopName: '\uc81c\uc774\ube14\ub9b0',
+			title: '\ubbf8\uc728\ud6ccsk *\uc8fc\ubb38\ud3ed\uc8fc*',
+			url:
+				'http://j-blin.com/product/detail.html?product_no=4391&cate_no=54&display_group=1&utm_source=styleshare&cafe_mkt=styleshare',
+		},
+		{
+			id: 'modernacc_3068',
+			image: {
+				id: 40991139,
+				url: 'https://usercontents-c.styleshare.kr/images/40991139/original',
+			},
+			liked: false,
+			likedCount: 0,
+			mobileUrl:
+				'http://m.modernacc.net/product/detail.html?product_no=3068&cate_no=42&display_group=1&utm_source=styleshare&cafe_mkt=styleshare',
+			price: 43900,
+			shopId: 'modernacc',
+			shopName: '\ubaa8\ub358\uc545',
+			title:
+				'\ub77c\ud30c\uc5d8 \ud074\ub798\uc2dd \ubca8\ud2b8 \ub77c\uc6b4\ub4dc \uc26c\ud3f0 \ud2b8\uc784 7\ubd80 \ubbf8\ub514\uc6d0\ud53c\uc2a4 \ud558\uac1d\ub8e9 (4color)',
+			url:
+				'http://modernacc.net/product/detail.html?product_no=3068&cate_no=42&display_group=1&utm_source=styleshare&cafe_mkt=styleshare',
+		},
+		{
+			id: 'modernacc_3063',
+			image: {
+				id: 40991127,
+				url: 'https://usercontents-c.styleshare.kr/images/40991127/original',
+			},
+			liked: false,
+			likedCount: 0,
+			mobileUrl:
+				'http://m.modernacc.net/product/detail.html?product_no=3063&cate_no=43&display_group=1&utm_source=styleshare&cafe_mkt=styleshare',
+			price: 25900,
+			shopId: 'modernacc',
+			shopName: '\ubaa8\ub358\uc545',
+			title:
+				'[\ud004\uad7f/\ud3ed\uc2e0] \uc5d0\uc774\uc9c0 \uac00\uc8fd \uc2ec\ud50c \uae30\ubcf8 \ud50c\ub7ab \ub048 \ub85c\ud37c \uc625\uc2a4\ud37c\ub4dc\ud654 (230-250/\ud654\uc774\ud2b8,\ube14\ub799)',
+			url:
+				'http://modernacc.net/product/detail.html?product_no=3063&cate_no=43&display_group=1&utm_source=styleshare&cafe_mkt=styleshare',
+		},
+	],
+	pagination: {
+		next:
+			'https://gist.githubusercontent.com/styleshare-automation/c0ecddc18a84270128f55efeace2311b/raw/ad0f0697bfe8673fde98ac7bce822ccf5f89a75b/second-goods-response.json',
+	},
+};
+
+export const ranks = {
+	data: [
+		{
+			description: '10\ub300, \ub7ec\ube14\ub9ac',
+			id: 'sonyunara',
+			image: {
+				id: 32108187,
+				url: 'https://usercontents-c.styleshare.kr/images/32108187/original',
+			},
+			liked: false,
+			name: '\uc18c\ub140\ub098\ub77c',
+			url: 'http://www.sonyunara.com?utm_source=styleshare&cafe_mkt=styleshare',
+		},
+		{
+			description: '10\ub300, \uce90\uc8fc\uc5bc',
+			id: 'gosister',
+			image: {
+				id: 34571457,
+				url: 'https://usercontents-c.styleshare.kr/images/34571457/original',
+			},
+			liked: false,
+			name: '\uc5b8\ub2c8\uac00\uac04\ub2e4',
+			url: 'http://www.gosister.co.kr?utm_source=styleshare&cafe_mkt=styleshare',
+		},
+		{
+			description: '10\ub300,   20\ub300, \ubca0\uc774\uc9c1',
+			id: '66girls',
+			image: {
+				id: 32106453,
+				url: 'https://usercontents-c.styleshare.kr/images/32106453/original',
+			},
+			liked: false,
+			name: '\uc721\uc721\uac78\uc988',
+			url: 'http://www.66girls.co.kr?utm_source=styleshare&cafe_mkt=styleshare',
+		},
+		{
+			description: '10\ub300,   20\ub300, \uc2ec\ud50c',
+			id: 'banharu',
+			image: {
+				id: 32105810,
+				url: 'https://usercontents-c.styleshare.kr/images/32105810/original',
+			},
+			liked: false,
+			name: '\ubc18\ud558\ub8e8',
+			url: 'http://www.banharu.com?utm_source=styleshare&cafe_mkt=styleshare',
+		},
+		{
+			description: '10\ub300, \ubca0\uc774\uc9c1',
+			id: 'hotping',
+			image: {
+				id: 32106328,
+				url: 'https://usercontents-c.styleshare.kr/images/32106328/original',
+			},
+			liked: false,
+			name: '\ud56b\ud551',
+			url: 'http://www.hotping.co.kr?utm_source=styleshare&cafe_mkt=styleshare',
+		},
+		{
+			description: '10\ub300, \uce90\uc8fc\uc5bc',
+			id: 'merongshop',
+			image: {
+				id: 32106535,
+				url: 'https://usercontents-c.styleshare.kr/images/32106535/original',
+			},
+			liked: false,
+			name: '\uba54\ub871\uc0f5',
+			url: 'http://www.merongshop.com?utm_source=styleshare&cafe_mkt=styleshare',
+		},
+		{
+			description: '10\ub300, \uc720\ub2c8\ud06c',
+			id: 'dejou',
+			image: {
+				id: 32106399,
+				url: 'https://usercontents-c.styleshare.kr/images/32106399/original',
+			},
+			liked: false,
+			name: '\ub370\uc96c',
+			url: 'http://dejou.co.kr?utm_source=styleshare&cafe_mkt=styleshare',
+		},
+		{
+			description: '10\ub300,   20\ub300, \ub7ec\ube14\ub9ac',
+			id: 'chuu',
+			image: {
+				id: 25563120,
+				url: 'https://usercontents-c.styleshare.kr/images/25563120/original',
+			},
+			liked: false,
+			name: '\uce04',
+			url: 'http://www.chuu.co.kr?utm_source=styleshare&cafe_mkt=styleshare',
+		},
+		{
+			description: '10\ub300,   20\ub300, \ubca0\uc774\uc9c1',
+			id: 'dailymonday',
+			image: {
+				id: 25563086,
+				url: 'https://usercontents-c.styleshare.kr/images/25563086/original',
+			},
+			liked: false,
+			name: '\ub370\uc77c\ub9ac\uba3c\ub370\uc774',
+			url: 'http://www.dailymonday.com?utm_source=styleshare&cafe_mkt=styleshare',
+		},
+		{
+			description: '10\ub300,   20\ub300, \ub7ec\ube14\ub9ac',
+			id: 'cherrykoko',
+			image: {
+				id: 32106663,
+				url: 'https://usercontents-c.styleshare.kr/images/32106663/original',
+			},
+			liked: false,
+			name: '\uccb4\ub9ac\ucf54\ucf54',
+			url: 'http://www.cherrykoko.com?utm_source=styleshare&cafe_mkt=styleshare',
+		},
+	],
+	pagination: {
+		next:
+			'https://gist.githubusercontent.com/styleshare-automation/c0ecddc18a84270128f55efeace2311b/raw/b3db8e961b6b9d51603c1cd6c795f03ad20d6ac9/second-mall-response.json',
+	},
+};
