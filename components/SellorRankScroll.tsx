fcd48c009ff224c9d82e758629a397bbb4ccc4c1
myno <minho.yoo.dev@gmail.com>
2021-04-20 17:51:04 +0900

    ranks page api 적용

--- components/SellorRankScroll.tsx
+++ components/SellorRankScroll.tsx
@@ -1,12 +1,34 @@
 import { Sellor } from '../interfaces/sellor.interface';
+import { useEffect, useRef } from 'react';
 import ContainerStyled from '../styled/container.styled';
 import SellorRankList from '../styled/sellor-rank-list';
 import SellorRankListItem from '../styled/sellor-rank-list-item';
+import LoadingElement from '../styled/loading-element.styled';
 
 interface SellorRankScrollProps {
 	data: Sellor[];
+	onLoadMore: IntersectionObserverCallback;
+	isLoading: boolean;
 }
-const SellorRankScroll = ({ data }: SellorRankScrollProps) => {
+const SellorRankScroll = ({ data, onLoadMore, isLoading }: SellorRankScrollProps) => {
+	const bottomElement = useRef<HTMLDivElement>(null);
+
+	useEffect(() => {
+		let observer: IntersectionObserver;
+		if (bottomElement.current && onLoadMore) {
+			observer = new IntersectionObserver(onLoadMore, {
+				root: null,
+				threshold: 0.1,
+				rootMargin: '10px',
+			});
+
+			observer.observe(bottomElement.current);
+		}
+		return () => {
+			bottomElement?.current && observer.unobserve(bottomElement.current);
+		};
+	}, [bottomElement, onLoadMore]);
+
 	return (
 		<ContainerStyled>
 			<SellorRankList>
@@ -25,6 +47,8 @@ const SellorRankScroll = ({ data }: SellorRankScrollProps) => {
 					</SellorRankListItem>
 				))}
 			</SellorRankList>
+			<div ref={bottomElement} />
+			{isLoading && <LoadingElement />}
 		</ContainerStyled>
 	);
 };
--- pages/ranks.tsx
+++ pages/ranks.tsx
@@ -1,10 +1,49 @@
+import { useCallback, useState } from 'react';
 import SellorRankScroll from '../components/SellorRankScroll';
-import { ranks } from '../__mock_data';
+import { Sellor, ResponseSellors } from '../interfaces/sellor.interface';
+import { Pagination } from '../interfaces/default.interface';
+import { GetServerSideProps } from 'next';
 
-interface RanksPageProps {}
+const FIRST_MALLS_API_URL =
+	'https://gist.githubusercontent.com/styleshare-automation/c0ecddc18a84270128f55efeace2311b/raw/ad0f0697bfe8673fde98ac7bce822ccf5f89a75b/first-mall-response.json';
+interface RanksPageProps {
+	sellors: Sellor[];
+	pagination: Pagination;
+}
 
-const RanksPage = (props: RanksPageProps) => {
-	return <SellorRankScroll data={ranks.data} />;
+const RanksPage = ({ sellors: _sellors, pagination }: RanksPageProps) => {
+	const [isLoading, setIsLoading] = useState(false);
+	const [sellors, setSellors] = useState(_sellors || []);
+	const [next, setNext] = useState(pagination?.next || '');
+
+	const handleLoadMore = useCallback(
+		async (entries: IntersectionObserverEntry[]) => {
+			if (entries[0].isIntersecting && !isLoading && !!next) {
+				try {
+					setIsLoading(true);
+					const response = await fetch(next);
+					const data = JSON.parse(await response.text()) as ResponseSellors;
+					setSellors([...sellors, ...data.data]);
+					setNext(data.pagination.next || '');
+				} catch (error) {
+					console.log(error);
+				} finally {
+					setIsLoading(false);
+				}
+			}
+		},
+		[next, sellors, isLoading],
+	);
+
+	return (
+		<SellorRankScroll data={sellors} onLoadMore={handleLoadMore} isLoading={!!next && isLoading} />
+	);
+};
+
+export const getServerSideProps: GetServerSideProps = async () => {
+	const response = await fetch(FIRST_MALLS_API_URL);
+	const data = JSON.parse(await response.text()) as ResponseSellors;
+	return { props: { sellors: data.data, pagination: data.pagination } };
 };
 
 export default RanksPage;
