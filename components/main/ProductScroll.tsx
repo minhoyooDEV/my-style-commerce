a704104d1163f7293afe007b654dda229b8e34e5
myno <minho.yoo.dev@gmail.com>
2021-04-20 16:36:53 +0900

    상품리스트 API적용

--- components/main/ProductScroll.tsx
+++ components/main/ProductScroll.tsx
@@ -1,19 +1,44 @@
+import { useEffect, useRef } from 'react';
 import { Product } from '../../interfaces/product.interface';
 import ContainerStyled from '../../styled/container.styled';
+import LoadingElement from '../../styled/loading-element.styled';
 import MainProductGridList from '../../styled/main-product-grid-list';
 import ProductScrollItem from './ProductScrollItem';
 
 interface ProductScrollProps {
 	data: Product[];
+	onLoadMore?: IntersectionObserverCallback;
+	isLoading?: boolean;
 }
-const ProductScroll = ({ data }: ProductScrollProps) => {
+const ProductScroll = ({ data, onLoadMore, isLoading }: ProductScrollProps) => {
+	const scrollRef = useRef<HTMLUListElement>(null);
+	const bottomElement = useRef<HTMLDivElement>(null);
+
+	useEffect(() => {
+		let observer: IntersectionObserver;
+		if (bottomElement.current && onLoadMore) {
+			observer = new IntersectionObserver(onLoadMore, {
+				root: null,
+				threshold: 0.1,
+				rootMargin: '10px',
+			});
+
+			observer.observe(bottomElement.current);
+		}
+		return () => {
+			bottomElement?.current && observer.unobserve(bottomElement.current);
+		};
+	}, [bottomElement, onLoadMore]);
+
 	return (
 		<ContainerStyled>
-			<MainProductGridList>
+			<MainProductGridList ref={scrollRef}>
 				{data.map(d => (
 					<ProductScrollItem key={d.id} data={d} />
 				))}
+				<div ref={bottomElement} />
 			</MainProductGridList>
+			{isLoading && <LoadingElement />}
 		</ContainerStyled>
 	);
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,51 @@
+import { GetServerSideProps } from 'next';
+import { useCallback, useState } from 'react';
 import ProductScroll from '../components/main/ProductScroll';
-import { Product } from '../interfaces/product.interface';
-import { products } from '../__mock_data';
+import { Pagination } from '../interfaces/default.interface';
+import { Product, ResponseProducts } from '../interfaces/product.interface';
 
-export default function Home() {
-	return <ProductScroll data={products.data as Product[]} />;
+const FIRST_GOODS_API_URL =
+	'https://gist.githubusercontent.com/styleshare-automation/c0ecddc18a84270128f55efeace2311b/raw/1cc13eeb3492199ead7123dc508cd767d12c47ba/first-goods-response.json';
+interface MainPageProps {
+	products: Product[];
+	pagination: Pagination;
 }
+
+const MainPage = ({ products: _products, pagination }: MainPageProps) => {
+	const [isLoading, setIsLoading] = useState(false);
+	const [products, setProducts] = useState(_products || []);
+	const [next, setNext] = useState(pagination?.next || '');
+
+	const handleLoadMore = useCallback(
+		async (entries: IntersectionObserverEntry[]) => {
+			// console.log(entries[0].isIntersecting && !isLoading && !!next);
+			if (entries[0].isIntersecting && !isLoading && !!next) {
+				try {
+					setIsLoading(true);
+					const response = await fetch(next);
+					const data = JSON.parse(await response.text()) as ResponseProducts;
+					// console.log('data', data);
+					setProducts([...products, ...data.data]);
+					// console.log('next', data.pagination.next);
+					setNext(data.pagination.next || '');
+				} catch (error) {
+					console.log(error);
+				} finally {
+					setIsLoading(false);
+				}
+			}
+		},
+		[next, products, isLoading],
+	);
+	return (
+		<ProductScroll data={products} onLoadMore={handleLoadMore} isLoading={!!next && isLoading} />
+	);
+};
+
+export const getServerSideProps: GetServerSideProps = async () => {
+	const response = await fetch(FIRST_GOODS_API_URL);
+	const data = JSON.parse(await response.text()) as ResponseProducts;
+	return { props: { products: data.data, pagination: data.pagination } };
+};
+
+export default MainPage;
