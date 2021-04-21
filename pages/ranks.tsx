02b4d3ec834a599a9f444e4c9737012e8640d054
myno <minho.yoo.dev@gmail.com>
2021-04-21 13:01:37 +0900

    ranks page sellor store 적용

--- pages/ranks.tsx
+++ pages/ranks.tsx
@@ -1,49 +1,49 @@
+import { GetServerSideProps } from 'next';
 import { useCallback, useState } from 'react';
 import SellorRankScroll from '../components/SellorRankScroll';
-import { Sellor, ResponseSellors } from '../interfaces/sellor.interface';
-import { Pagination } from '../interfaces/default.interface';
-import { GetServerSideProps } from 'next';
+import { ResponseSellors } from '../interfaces/sellor.interface';
+import { useStore } from '../modules/stores';
 
-const FIRST_MALLS_API_URL =
-	'https://gist.githubusercontent.com/styleshare-automation/c0ecddc18a84270128f55efeace2311b/raw/ad0f0697bfe8673fde98ac7bce822ccf5f89a75b/first-mall-response.json';
-interface RanksPageProps {
-	sellors: Sellor[];
-	pagination: Pagination;
-}
+interface RanksPageProps {}
 
-const RanksPage = ({ sellors: _sellors, pagination }: RanksPageProps) => {
+const RanksPage = ({}: RanksPageProps) => {
 	const [isLoading, setIsLoading] = useState(false);
-	const [sellors, setSellors] = useState(_sellors || []);
-	const [next, setNext] = useState(pagination?.next || '');
+	const { sellorStore } = useStore();
 
 	const handleLoadMore = useCallback(
 		async (entries: IntersectionObserverEntry[]) => {
-			if (entries[0].isIntersecting && !isLoading && !!next) {
+			if (entries[0].isIntersecting && !isLoading && sellorStore.next) {
 				try {
 					setIsLoading(true);
-					const response = await fetch(next);
-					const data = JSON.parse(await response.text()) as ResponseSellors;
-					setSellors([...sellors, ...data.data]);
-					setNext(data.pagination.next || '');
+					await sellorStore.loadSellors();
 				} catch (error) {
-					console.log(error);
+					console.error({ error });
 				} finally {
 					setIsLoading(false);
 				}
 			}
 		},
-		[next, sellors, isLoading],
+		[sellorStore.next, sellorStore.list, isLoading],
 	);
 
 	return (
-		<SellorRankScroll data={sellors} onLoadMore={handleLoadMore} isLoading={!!next && isLoading} />
+		<SellorRankScroll
+			data={sellorStore.list}
+			onLoadMore={handleLoadMore}
+			isLoading={!!sellorStore.next && isLoading}
+		/>
 	);
 };
 
 export const getServerSideProps: GetServerSideProps = async () => {
-	const response = await fetch(FIRST_MALLS_API_URL);
-	const data = JSON.parse(await response.text()) as ResponseSellors;
-	return { props: { sellors: data.data, pagination: data.pagination } };
+	// console.log('RanksPage - getServerSideProps');
+	const response = await fetch(process.env.NEXT_PUBLIC_MALLS_FIRST_REQ_API);
+	const data = (await response.json()) as ResponseSellors;
+	return {
+		props: {
+			sellorStore: { list: data.data, pagination: data.pagination },
+		},
+	};
 };
 
 export default RanksPage;
