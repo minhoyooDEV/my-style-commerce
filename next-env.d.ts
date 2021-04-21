bd1dae185d90bab686f22849d881bc57d9e2991d
myno <minho.yoo.dev@gmail.com>
2021-04-21 12:45:37 +0900

    api env 변수 추가

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,9 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+
+declare namespace NodeJS {
+	export interface ProcessEnv {
+		NEXT_PUBLIC_GOODS_FIRST_REQ_API: string;
+		NEXT_PUBLIC_MALLS_FIRST_REQ_API: string;
+	}
+}
