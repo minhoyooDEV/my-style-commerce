26eae08370d3041e4f3ca4b804f68d166f82d715
myno <minho.yoo.dev@gmail.com>
2021-04-21 12:46:48 +0900

    state store 추가

---
+++ modules/stores.tsx
@@ -0,0 +1,52 @@
+import React, { useMemo } from 'react';
+import { enableStaticRendering } from 'mobx-react';
+import { configure } from 'mobx';
+
+const isServer = typeof window === 'undefined';
+// ssr issuese
+enableStaticRendering(isServer);
+
+configure({
+	useProxies: 'never',
+});
+
+type initializeStoreProps = {};
+
+const makeStore = () => ({});
+
+type TStore = ReturnType<typeof makeStore>;
+
+let store: TStore;
+
+function initializeStore(initialData: initializeStoreProps) {
+	const _store: TStore = store ?? makeStore();
+
+	// If your page has Next.js data fetching methods that use a Mobx store, it will
+	// get hydrated here, check `pages/ssg.js` and `pages/ssr.js` for more details
+	if (initialData) {
+	}
+	// For SSG and SSR always create a new store
+	if (typeof window === 'undefined') return _store;
+	// Create the store once in the client
+	if (!store) store = _store;
+
+	return _store;
+}
+
+export const storeContext = React.createContext<TStore | null>(null);
+interface StoreProviderProps {
+	children: React.ReactNode;
+	store: TStore | null;
+}
+export const StoreProvider = ({ store, children }: StoreProviderProps) => (
+	<storeContext.Provider value={store}>{children}</storeContext.Provider>
+);
+
+export const useStore = () => {
+	return React.useContext(storeContext) as TStore;
+};
+
+export const useInitializeStore = (initialStore: initializeStoreProps) => {
+	const store = useMemo(() => initializeStore(initialStore), [initialStore]);
+	return store;
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,25 @@
 import { AppProps } from 'next/dist/next-server/lib/router/router';
 import { ThemeProvider } from 'styled-components';
 import Header from '../components/Header';
+import { StoreProvider, useInitializeStore } from '../modules/stores';
 import ComponentsWrapperStyled from '../styled/components-wrapper.styled';
 import { basicTheme } from '../styled/themes';
 
 import '../styles/globals.css';
 
 function App({ Component, pageProps }: AppProps) {
+	const initialProp = {};
+	const store = useInitializeStore(initialProp);
+
 	return (
-		<ThemeProvider theme={basicTheme}>
-			<Header />
-			<ComponentsWrapperStyled>
-				<Component {...pageProps} />
-			</ComponentsWrapperStyled>
-		</ThemeProvider>
+		<StoreProvider store={store}>
+			<ThemeProvider theme={basicTheme}>
+				<Header />
+				<ComponentsWrapperStyled>
+					<Component {...pageProps} />
+				</ComponentsWrapperStyled>
+			</ThemeProvider>
+		</StoreProvider>
 	);
 }
 
