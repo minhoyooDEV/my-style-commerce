1bbe05cd6e37c04fd65e1cb820618251f65ddc04
myno <minho.yoo.dev@gmail.com>
2021-04-21 14:20:06 +0900

    저해상도 관련 스타일 조정

--- styled/container.styled.tsx
+++ styled/container.styled.tsx
@@ -4,14 +4,20 @@ import mediaquery from '../utiles/mediaquery';
 interface ContainerStyledProps {
 	noGutter?: boolean;
 }
-const ContainerStyled = styled.div<ContainerStyledProps>`
-	margin-left: auto;
-	margin-right: auto;
-	padding-left: ${({ theme, noGutter }) => (noGutter ? '0' : theme['spacing-2'])};
-	padding-right: ${({ theme, noGutter }) => (noGutter ? '0' : theme['spacing-2'])};
-	max-width: ${({ theme }) => theme.contentsMaxWith};
-	background-color: white;
-`;
+const ContainerStyled = styled.div<ContainerStyledProps>(
+	({ theme, noGutter }) => css`
+		margin-left: auto;
+		margin-right: auto;
+		padding-left: ${noGutter ? '0' : theme.spacing};
+		padding-right: ${noGutter ? '0' : theme.spacing};
+		${mediaquery.xs(`
+			padding-left: ${noGutter ? '0' : theme['spacing-2']};
+			padding-right: ${noGutter ? '0' : theme['spacing-2']};
+		`)}
+		max-width: ${theme.contentsMaxWith};
+		background-color: white;
+	`,
+);
 export default ContainerStyled;
 
 export const ResponsiveContainerStyled = styled(ContainerStyled)(
--- utiles/mediaquery.ts
+++ utiles/mediaquery.ts
@@ -3,24 +3,23 @@
  */
 
 const size = {
-	xs: '320px',
+	xs: '321px',
 	sm: '576px',
 	md: '768px',
 	lg: '992px',
 	xl: '1200px',
 };
 
+// https://codesandbox.io/embed/dry-media-queries-styled-components-typescript-8dwwu
 /**
  * @param {{
+ * xs: string,
  * sm: string,
  * md: string,
  * lg: string,
  * xl: string
  * }} size
  */
-
-// https://codesandbox.io/embed/dry-media-queries-styled-components-typescript-8dwwu
-
 const mediaquery = (Object.keys(size) as Array<keyof typeof size>).reduce((acc, key) => {
 	acc[key] = (style: string) => `@media (min-width: ${size[key]}) { ${style} }`;
 	return acc;
